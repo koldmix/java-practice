34a8e2424f55f13cec33e760e7e7de456449981f
K Mikhail <mirokol5@mail.ru>
2026-05-21 21:48:46 +0400

    Созаю новую ветку и пишу еще строку

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Вассап мабой");
         System.out.println("Пробую amend");
+
+        System.out.println("Добавил новую ветку");
+
     }
 }
