2d11236d79cbd78102ca2159d7b7e7d18b34523b
K Mikhail <mirokol5@mail.ru>
2026-05-22 15:30:02 +0400

    Обнуление

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Вассап мабой");
-        System.out.println("Пробую amend");
-
-        System.out.println("Это НОВАЯ строчка из main, которая создаст конфликт");
-
-        System.out.println("Добавил новую веткуq");
-        System.out.println("Добавил новую веткуq");
-
 
     }
 }
