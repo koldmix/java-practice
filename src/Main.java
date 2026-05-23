c07fbd7234c7aa4a525e9ad8388deba29a1a6bea
K Mikhail <mirokol5@mail.ru>
2026-05-23 17:46:32 +0400

    Работает, но при просмотре задач выводит пустую строку

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,48 @@ public class Main {
     public static void main(String[] args) {
 
         int userInpMenu;
+        int countTasks = 1;
+        String[] arrTasks = new String[countTasks];
+        String[] afterAdd = {};
+
         do {
             showMenu();
             Scanner scannerMenu = new Scanner(System.in);
             userInpMenu = scannerMenu.nextInt();
-            if (userInpMenu != 0){
-                switch (userInpMenu){
-                    case 1 -> addTask();
-                    case 2 -> showTasks();
+            if (userInpMenu != 0) {
+                switch (userInpMenu) {
+                    case 1 -> {
+                        addTask(arrTasks);
+                        countTasks++;
+                        afterAdd = new String[]{Arrays.toString(arrTasks)};
+                    }
+                    case 2 -> showTasks(afterAdd);
                 }
             }
-        }while (userInpMenu != 0);
+        } while (userInpMenu != 0);
     }
 
-    public static void addTask() {
+    public static String[] addTask(String[] arr) {
         System.out.println("Введите описание задачи: ");
         Scanner userInTask = new Scanner(System.in);
         String userTask = userInTask.nextLine();
 
-        String[] addTask = Arrays.copyOf(showTasks(), showTasks().length + 1);
-        addTask[showTasks().length] = userTask;
+        String[] addTask = new String[arr.length];
+        addTask[arr.length - 1] = userTask;
 
         System.out.println("Задача добавлена!" + "\n");
+        return addTask;
     }
 
-    public static String[] showTasks(String[] strTasks) {
+    public static void showTasks(String[] strTasks) {
         System.out.println("Список задач:");
-        if (strTasks.length == 0){
+        if (strTasks.length == 0) {
             System.out.println("Задач нет" + "\n");
         }
         for (String arrTask : strTasks) {
-            System.out.println(arrTask);
+            System.out.println(arrTask + "\n");
         }
-        return strTasks;
+//        return strTasks;
     }
 
     public static void showMenu() {
@@ -51,8 +60,4 @@ public class Main {
                 "Выберите пункт меню:\n");
     }
 
-//    public static String[] arr(){
-//        String[] arr = {};
-//        return arr;
-//    }
 }
