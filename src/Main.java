513c76623125bd49e82b89e9c6870c882f5ab913
K Mikhail <mirokol5@mail.ru>
2026-05-26 17:44:37 +0400

    Вынес поля на уровень класса, изменилввод строки, добавил мод.доступа

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String[] arrTasks = new String[100];
+    private static int countTasks = 0;
+
     public static void main(String[] args) {
 
         int userInpMenu = -1;
-        String[] arrTasks = new String[100];
-        int countTasks = 0;
-
-        Scanner scannerMenu = new Scanner(System.in);
 
         do {
             showMenu();
-            String input = scannerMenu.nextLine();
+            String input = scanner.nextLine();
 
             try {
-             userInpMenu = Integer.parseInt(input);
+                userInpMenu = Integer.parseInt(input);
             } catch (NumberFormatException e) {
                 System.out.println("Введите число");
                 continue;
@@ -26,96 +26,94 @@ public class Main {
             if (userInpMenu != 0) {
                 switch (userInpMenu) {
                     case 1 -> {
-                        addTask(arrTasks, countTasks);
+                        addTask();
                         countTasks++;
                     }
-                    case 2 -> showTasks(arrTasks, countTasks);
-                    case 3 -> countTasks = deleteTask(arrTasks, countTasks);
-                    case 4 -> markAsComplete(arrTasks);
+                    case 2 -> showTasks();
+                    case 3 -> countTasks = deleteTask();
+                    case 4 -> markAsComplete();
                 }
             }
         } while (userInpMenu != 0);
     }
 
-    public static void addTask(String[] arr, int realCountTasks) {
-        if (realCountTasks >= 99){
+    private static void addTask() {
+        if (countTasks >= 99) {
             System.out.println("Список задач заполнен.");
             return;
         }
 
         System.out.println("Введите описание задачи: ");
-        Scanner userInTask = new Scanner(System.in);
-        String userTask = userInTask.nextLine();
+        String userTask = scanner.nextLine();
 
-        arr[realCountTasks] = "[ ]" + userTask;
+        arrTasks[countTasks] = "[ ]" + userTask;
 
         System.out.println("Задача добавлена!" + "\n");
     }
 
-    public static void showTasks(String[] strTasks, int realCountTasks) {
-        if (realCountTasks == 0) {
+    private static void showTasks() {
+        if (countTasks == 0) {
             System.out.println("Задач нет" + "\n");
             return;
         }
         System.out.println("Список задач:");
 
-        for (int i = 0; i < realCountTasks; i++) {
-            if (strTasks[i] != null)
-                System.out.print(i + 1 + ". " + strTasks[i] + "\n");
+        for (int i = 0; i < countTasks; i++) {
+            if (arrTasks[i] != null)
+                System.out.print(i + 1 + ". " + arrTasks[i] + "\n");
         }
         System.out.println();
     }
 
-    public static int deleteTask(String[] arrTasks, int realCountTasks){
+    private static int deleteTask() {
         try {
             System.out.println("Введите номер задачи для удаления: ");
-            Scanner userInDelete = new Scanner(System.in);
-            int userDel = userInDelete.nextInt();
+            int userDel = scanner.nextInt();
 
-            if (arrTasks[userDel - 1] != null){
-                for (int i = userDel - 1; i < realCountTasks; i++) {
+            if (arrTasks[userDel - 1] != null) {
+                for (int i = userDel - 1; i < countTasks; i++) {
                     arrTasks[i] = arrTasks[i + 1];
                 }
-            }else {
+            } else {
                 System.out.println("Такой задачи не существует");
             }
             System.out.println("Задача " + "\"" + arrTasks[userDel - 1] + "\"" + " удалена!");
         } catch (Exception e) {
             System.out.println("Такой задачи не существует");
         }
-        return realCountTasks - 1;
+        return countTasks - 1;
     }
 
-    public static void markAsComplete(String[] arrTask){
+    private static void markAsComplete() {
         try {
 
             System.out.println("Введите номер задачи для отметки: ");
-            Scanner userMarkCompetition = new Scanner(System.in);
-            int userMark = userMarkCompetition.nextInt();
+            int userMark = scanner.nextInt();
 
             if (userMark == 0) {
                 System.out.println("Такой задачи нет");
             }
 
-            if (arrTask[userMark - 1] != null) {
-                arrTask[userMark - 1] = arrTask[userMark - 1].replace("[ ]", "[x]");
-                System.out.println("Задача " + "\"" + arrTask[userMark - 1] + "\"" + " отмечена как выполненная!");
-            }else
+            if (arrTasks[userMark - 1] != null) {
+                arrTasks[userMark - 1] = arrTasks[userMark - 1].replace("[ ]", "[x]");
+                System.out.println("Задача " + "\"" + arrTasks[userMark - 1] + "\"" + " отмечена как выполненная!");
+            } else
                 System.out.println("Такой задачи нет");
         } catch (Exception e) {
             System.out.println("Такой задачи не существует");
         }
     }
 
-    public static void showMenu() {
-        System.out.println(">>> Меню:\n" +
-                "1. Добавить задачу\n" +
-                "2. Показать все задачи\n" +
-                "3. Удалить задачу (по номеру)\n" +
-                "4. Отметить задачу как выполненную\n" +
-                "0. Выход\n" +
-                "\n" +
-                "Выберите пункт меню:\n");
+    private static void showMenu() {
+        System.out.println("""
+                >>> Меню:\s
+                1. Добавить задачу
+                2. Показать все задачи
+                3. Удалить задачу (по номеру)
+                4. Отметить задачу как выполненную
+                0. Выход
+                
+                Выберите пункт меню:
+                """);
     }
-
 }
