8217aa786b927254e9f233135dfe6335abaadeca
K Mikhail <mirokol5@mail.ru>
2026-05-26 16:43:15 +0400

    Всё готово)))

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int userInpMenu;
+        int userInpMenu = -1;
         String[] arrTasks = new String[100];
         int countTasks = 0;
 
+        Scanner scannerMenu = new Scanner(System.in);
+
         do {
             showMenu();
-            Scanner scannerMenu = new Scanner(System.in);
-            userInpMenu = scannerMenu.nextInt();
+            String input = scannerMenu.nextLine();
+
+            try {
+             userInpMenu = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Введите число");
+                continue;
+            }
+
+            if (userInpMenu > 4 || userInpMenu < 0) {
+                System.out.println("Такого пункта меню нет");
+            }
             if (userInpMenu != 0) {
                 switch (userInpMenu) {
                     case 1 -> {
@@ -18,7 +30,7 @@ public class Main {
                         countTasks++;
                     }
                     case 2 -> showTasks(arrTasks, countTasks);
-                    case 3 -> deleteTask(arrTasks, countTasks);
+                    case 3 -> countTasks = deleteTask(arrTasks, countTasks);
                     case 4 -> markAsComplete(arrTasks);
                 }
             }
@@ -26,6 +38,11 @@ public class Main {
     }
 
     public static void addTask(String[] arr, int realCountTasks) {
+        if (realCountTasks >= 99){
+            System.out.println("Список задач заполнен.");
+            return;
+        }
+
         System.out.println("Введите описание задачи: ");
         Scanner userInTask = new Scanner(System.in);
         String userTask = userInTask.nextLine();
@@ -36,44 +53,58 @@ public class Main {
     }
 
     public static void showTasks(String[] strTasks, int realCountTasks) {
-        System.out.println("Список задач:");
-
         if (realCountTasks == 0) {
             System.out.println("Задач нет" + "\n");
             return;
         }
+        System.out.println("Список задач:");
 
         for (int i = 0; i < realCountTasks; i++) {
             if (strTasks[i] != null)
-//                System.out.print(i + 1 + ". " + "[ ] " + strTasks[i] + "\n");
                 System.out.print(i + 1 + ". " + strTasks[i] + "\n");
         }
         System.out.println();
     }
 
-    public static void deleteTask(String[] arrTasks, int realCountTasks){
-        System.out.println("Введите номер задачи для удаления: ");
-        Scanner userInDelete = new Scanner(System.in);
-        int userDel = userInDelete.nextInt();
+    public static int deleteTask(String[] arrTasks, int realCountTasks){
+        try {
+            System.out.println("Введите номер задачи для удаления: ");
+            Scanner userInDelete = new Scanner(System.in);
+            int userDel = userInDelete.nextInt();
 
-        if (arrTasks[userDel - 1] != null){
-            for (int i = userDel - 1; i < realCountTasks; i++) {
-                arrTasks[i] = arrTasks[i + 1];
+            if (arrTasks[userDel - 1] != null){
+                for (int i = userDel - 1; i < realCountTasks; i++) {
+                    arrTasks[i] = arrTasks[i + 1];
+                }
+            }else {
+                System.out.println("Такой задачи не существует");
             }
-        }else {
+            System.out.println("Задача " + "\"" + arrTasks[userDel - 1] + "\"" + " удалена!");
+        } catch (Exception e) {
             System.out.println("Такой задачи не существует");
         }
-
-        System.out.println("Задача " + "\"" + arrTasks[userDel - 1] + "\"" + " удалена!");
+        return realCountTasks - 1;
     }
 
     public static void markAsComplete(String[] arrTask){
-        System.out.println("Введите номер задачи для отметки: ");
-        Scanner userMarkCompetition = new Scanner(System.in);
-        int userMark = userMarkCompetition.nextInt();
+        try {
 
-        arrTask[userMark - 1] = arrTask[userMark - 1].replace("[ ]", "[x]");
-        System.out.println("Задача " + "\"" + arrTask[userMark - 1] + "\"" + " отмечена как выполненная!");
+            System.out.println("Введите номер задачи для отметки: ");
+            Scanner userMarkCompetition = new Scanner(System.in);
+            int userMark = userMarkCompetition.nextInt();
+
+            if (userMark == 0) {
+                System.out.println("Такой задачи нет");
+            }
+
+            if (arrTask[userMark - 1] != null) {
+                arrTask[userMark - 1] = arrTask[userMark - 1].replace("[ ]", "[x]");
+                System.out.println("Задача " + "\"" + arrTask[userMark - 1] + "\"" + " отмечена как выполненная!");
+            }else
+                System.out.println("Такой задачи нет");
+        } catch (Exception e) {
+            System.out.println("Такой задачи не существует");
+        }
     }
 
     public static void showMenu() {
